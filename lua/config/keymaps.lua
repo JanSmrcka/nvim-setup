83d20b8adf9671f2591948dca1f91e53471aa50f
Jan Smrčka <jan.smrck@gmail.com>
2024-02-22 15:56:24 +0100

    ...

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -25,8 +25,5 @@ keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
 -- split window
 keymap.set("n", "ss", ":split<Return>", opts)
 keymap.set("n", "sv", ":vsplit<Return>", opts)
--- move window
-keymap.set("n", "sh", "<C-w>h")
-keymap.set("n", "sk", "<C-w>k")
 keymap.set("n", "sj", "<C-w>j")
 keymap.set("n", "sl", "<C-w>l")
