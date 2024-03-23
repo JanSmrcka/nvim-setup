edfe93239e0bea8dd4c3c9e28daa5bb7adfdee7f
Jan Smrčka <jan.smrck@gmail.com>
2024-03-23 22:50:18 +0100

    setup lsp

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,73 @@
+local lsp_zero = require('lsp-zero')
+
+lsp_zero.on_attach(function(client, bufnr)
+	-- see :help lsp-zero-keybindings
+	-- to learn the available actions
+	lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
+-- to learn how to use mason.nvim with lsp-zero
+-- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guide/integrate-with-mason-nvim.md
+require('mason').setup({})
+require('mason-lspconfig').setup({
+	ensure_installed = {},
+	handlers = {
+		lsp_zero.default_setup,
+	},
+})
+
+
+require('mason-lspconfig').setup({
+	ensure_installed = {},
+	handlers = {
+		lsp_zero.default_setup,
+
+		--- replace `example_server` with the name of a language server
+		tsserver = function()
+			require('lspconfig').tsserver.setup({
+				---
+				-- in here you can add your own
+				-- custom configuration
+				---
+			})
+		end,
+		eslint = function()
+			require('lspconfig').eslint.setup({})
+		end,
+
+	},
+
+})
+
+local cmp = require('cmp')
+
+cmp.setup({
+  sources = {
+    {name = 'nvim_lsp'},
+  },
+  mapping = {
+    ['<C-y>'] = cmp.mapping.confirm({select = false}),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<Up>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
+    ['<Down>'] = cmp.mapping.select_next_item({behavior = 'select'}),
+    ['<C-p>'] = cmp.mapping(function()
+      if cmp.visible() then
+        cmp.select_prev_item({behavior = 'insert'})
+      else
+        cmp.complete()
+      end
+    end),
+    ['<C-n>'] = cmp.mapping(function()
+      if cmp.visible() then
+        cmp.select_next_item({behavior = 'insert'})
+      else
+        cmp.complete()
+      end
+    end),
+  },
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+})
--- lua/jendis/plugins.lua
+++ lua/jendis/plugins.lua
@@ -13,19 +13,45 @@ return require('packer').startup(function(use)
 		-- or                            , branch = '0.1.x',
 		requires = { {'nvim-lua/plenary.nvim'} }
 	}
-	
+
 	-- Color Schema
 	use ({ "rose-pine/neovim", as = "rose-pine" })
 
 	-- Neo Vim Tree sitter
 	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 	use('nvim-treesitter/playground')
-	
+
 	-- Harpoon
 	use('nvim-lua/plenary.nvim')
 	use('ThePrimeagen/harpoon')
-	
+
 	-- Undotree
 	use 'mbbill/undotree'
 	use 'tpope/vim-fugitive'
+
+	--LSP
+	use {
+		"williamboman/mason.nvim"
+	}
+	use {
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
+}
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v3.x',
+		requires = {
+			--- Uncomment the two plugins below if you want to manage the language servers from neovim
+			-- {'williamboman/mason.nvim'},
+			-- {'williamboman/mason-lspconfig.nvim'},
+
+			-- LSP Support
+			{'neovim/nvim-lspconfig'},
+			-- Autocompletion
+			{'hrsh7th/nvim-cmp'},
+			{'hrsh7th/cmp-nvim-lsp'},
+			{'L3MON4D3/LuaSnip'},
+		}
+	}
 end)
